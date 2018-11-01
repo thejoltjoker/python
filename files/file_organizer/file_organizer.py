a6afdaa0f46264f0c041afe682decdb4d063c432
Johannes Andersson <hello@thejoltjoker.com>
2018-11-01 19:25:01 +0100

    initial commit

---
+++ files/file_organizer/file_organizer.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# TODO group file formats
+"""
+organize_files_year.py
+Description of organize_files_year.py.
+"""
+import os
+import time
+import shutil
+
+# options
+source_folder = '/Users/thejoltjoker/desktop/org_test/unsorted'
+dest_folder = os.path.abspath(
+    os.path.join(source_folder, '../', 'sorted'))
+file_operation = 'move'
+
+print('Source folder is {}'.format(source_folder))
+print('Destination folder is {}'.format(dest_folder))
+print('File operation is {}'.format(source_folder))
+
+
+# grouping
+group_videos = ['mp4',
+                'avi',
+                'mpg',
+                'mov',
+                'mpeg',
+                'flv',
+                'mkv']
+
+group_pictures = ['jpg',
+                  'psd',
+                  'jpeg',
+                  'dng',
+                  'cr2',
+                  'arw',
+                  'tif',
+                  'tiff',
+                  'png']
+
+group_archives = ['zip',
+                  'tar',
+                  'gz',
+                  'rar',
+                  '7z',
+                  '7zip']
+
+group_docs = ['doc',
+              'gdoc',
+              'xls',
+              'docx',
+              'xlsx',
+              'pdf',
+              'txt',
+              'md']
+
+
+# create destination root
+if not os.path.exists(dest_folder):
+    os.makedirs(dest_folder)
+
+
+def get_file_type(file):
+    return file.split('.')[-1]
+
+
+def move_file(source, destination):
+    shutil.move(source, destination)
+
+
+def copy_file(source, destination):
+    shutil.copy2(source, destination)
+
+
+def create_destination_folder(type, year):
+    type_folder = os.path.join(dest_folder, type)
+    year_folder = os.path.join(type_folder, year)
+
+    if not os.path.exists(type_folder):
+        os.makedirs(type_folder)
+
+    if not os.path.exists(year_folder):
+        os.makedirs(year_folder)
+
+    return year_folder
+
+
+def main():
+    """docstring for main"""
+
+    for subdir, dirs, files in os.walk(source_folder):
+        for file in files:
+            file_type = get_file_type(file)
+            file_path = os.path.join(subdir, file)
+            file_year = time.strftime(
+                '%Y', time.gmtime(os.path.getmtime(file_path)))
+
+            # debug stuff
+            # print('File name: {}'.format(file))
+            # print('Path: {}'.format(file_path))
+            # print('Type: {}'.format(file_type))
+            # print('Year: {}'.format(file_year))
+
+            dest_folder = create_destination_folder(file_type, file_year)
+            dest_file_path = os.path.join(dest_folder, file)
+
+            if file_operation == 'move':
+                move_file(file_path, dest_file_path)
+                print("{src} moved to {dest}".format(
+                    src=file_path, dest=dest_file_path))
+            elif file_operation == 'copy':
+                copy_file(file_path, dest_file_path)
+                print("{src} copied to {dest}".format(
+                    src=file_path, dest=dest_file_path))
+            else:
+                print('invalid file operation')
+
+
+if __name__ == '__main__':
+    main()
