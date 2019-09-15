69ecdc591525e8f0449e1552980526743ee9febc
Johannes Andersson <hello@thejoltjoker.com>
2019-09-15 10:04:35 +0200

    add xxhash and verify after copy

--- files/camera_offload.py
+++ files/camera_offload.py
@@ -4,11 +4,11 @@
 script_name.py
 Description of script_name.py.
 """
-# TODO verify files after copy
 # TODO multiple destinations
 # TODO make it easier to customize
 import os
 import hashlib
+import xxhash
 import logging
 import datetime
 import shutil
@@ -35,13 +35,17 @@ class Offloader:
                         'INDEX.BDM',
                         'fseventsd-uuid',
                         '.dropbox.device',
-                        'AVIN0001.INT']
+                        'AVIN0001.INT',
+                        'mdb.bk',
+                        'mdb.db',
+                        'Get_started_with_GoPro.url']
 
     def setup_logger(self):
         # Setup logger
         logger = logging.getLogger(__name__)
         handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)-20s %(name)-10s %(levelname)-8s %(message)s', "%Y-%m-%d %H:%M:%S")
+        formatter = logging.Formatter(
+            '%(asctime)-20s %(name)-10s %(levelname)-8s %(message)s', "%Y-%m-%d %H:%M:%S")
 
         handler.setFormatter(formatter)
         logger.addHandler(handler)
@@ -57,9 +61,11 @@ class Offloader:
         source_file_list = self.get_file_list(self.source)
 
         # Get total file size to calculate percentage
-        total_file_size = sum([source_file_list[x]['size'] for x in source_file_list])
+        total_file_size = sum([source_file_list[x]['size']
+                               for x in source_file_list])
         total_transferred_size = 0
-        self.logger.info("Total file size: %s", self.convert_size(total_file_size))
+        self.logger.info("Total file size: %s",
+                         self.convert_size(total_file_size))
         self.logger.info("---\n")
 
         # Get start time to calculate remaining time
@@ -71,9 +77,11 @@ class Offloader:
             current_file = source_file_list[file_id]
 
             self.logger.info("Processing file %s/%s (~%s%%) | %s", file_id, len(source_file_list),
-                             round((total_transferred_size / total_file_size) * 100, 2),
+                             round((total_transferred_size /
+                                    total_file_size) * 100, 2),
                              current_file['name'])
 
+            # Skip file if it's in the exclude list
             if current_file['name'] in self.exclude:
                 self.logger.warning('Filename in exclude list %s, skipping',
                                     current_file['name'])
@@ -81,42 +89,52 @@ class Offloader:
                 # Check if file already exists at destination
                 skipped_files.append(current_file['name'])
 
+            # If file is not in include list move on to copy
             else:
                 while True:
                     if self.original_structure:
                         new_filename = current_file['name']
-                        dest_file_path = os.path.join(current_file['path'].replace(self.source, self.destination))
+                        dest_file_path = os.path.join(
+                            current_file['path'].replace(self.source, self.destination))
                     else:
                         # Create destination path
-                        new_filename = self.create_new_filename(current_file, incremental)
+                        new_filename = self.create_new_filename(
+                            current_file, incremental)
 
                         dest_file_path = os.path.join(self.destination,
-                                                      current_file['date'].strftime('%Y'),
-                                                      current_file['date'].strftime('%Y-%m-%d'),
+                                                      current_file['date'].strftime(
+                                                          '%Y'),
+                                                      current_file['date'].strftime(
+                                                          '%Y-%m-%d'),
                                                       new_filename)
 
                     # Check if file already exists at destination
                     if os.path.isfile(dest_file_path):
 
+                        self.logger.info(
+                            'File with the same name exists in destination, comparing checksums')
                         # Get file info for existing file
                         dest_file_info = self.get_file_info(dest_file_path)
 
                         # Compare checksums
-                        if current_file['checksum'] == dest_file_info['checksum']:
+                        # if current_file['checksum'] == dest_file_info['checksum']:
+                        if self.compare_checksums(current_file['path'], dest_file_info['path']):
                             self.logger.warning('File %s (%s) already exists in destination, skipping',
                                                 dest_file_info['name'],
                                                 current_file['name'])
-                            self.logger.warning('Checksums: %s (source)| %s (destination)',
-                                                current_file['checksum'],
-                                                dest_file_info['checksum'])
+                            # self.logger.warning('Checksums: %s (source)| %s (destination)',
+                            #                     current_file['checksum'],
+                            #                     dest_file_info['checksum'])
                             self.logger.debug('Source: %s', current_file)
-                            self.logger.debug('Destination: %s', dest_file_info)
+                            self.logger.debug(
+                                'Destination: %s', dest_file_info)
 
                             # Add file to skipped list
                             skipped_files.append(current_file['name'])
 
                             break
 
+                        # If file with same name exists but has mismatching checksums
                         else:
                             self.logger.warning(
                                 'File with same name %s (%s) already exists in destination, adding incremental',
@@ -126,7 +144,8 @@ class Offloader:
                                                 current_file['checksum'],
                                                 dest_file_info['checksum'])
                             self.logger.debug('Source: %s', current_file)
-                            self.logger.debug('Destination: %s', dest_file_info)
+                            self.logger.debug(
+                                'Destination: %s', dest_file_info)
 
                             # Increment number
                             incremental += 1
@@ -145,19 +164,36 @@ class Offloader:
                         # Copy file
                         try:
                             if self.move:
-                                self.logger.info("Moving %s", self.convert_size(current_file['size']))
+                                self.logger.info(
+                                    "Moving %s", self.convert_size(current_file['size']))
                             else:
-                                self.logger.info("Copying %s", self.convert_size(current_file['size']))
-                            self.logger.info("Source: %s", current_file['path'])
+                                self.logger.info(
+                                    "Copying %s", self.convert_size(current_file['size']))
+                            self.logger.info(
+                                "Source: %s", current_file['path'])
                             self.logger.info("Destination: %s", dest_file_path)
+
+                            # Get checksum
+                            # current_file['checksum'] = self.get_file_checksum_xxhash(
+                            #     current_file['path'])
 
                             if self.dryrun:
                                 self.logger.info("DRYRUN ENABLED, NOT COPYING")
                             else:
                                 if self.move:
-                                    self.move_file(current_file['path'], dest_file_path)
+                                    self.move_file(
+                                        current_file['path'], dest_file_path)
                                 else:
-                                    self.copy_file(current_file['path'], dest_file_path)
+                                    self.copy_file(
+                                        current_file['path'], dest_file_path)
+
+                                # Verify transferred file
+                                self.logger.info(
+                                    'Verifying transferred file')
+                                if self.compare_checksums(
+                                        current_file['path'], dest_file_path):
+                                    self.logger.info(
+                                        'File %s transferred successfully', current_file['name'])
 
                             # Add file to transferred list
                             transferred_files.append(current_file['name'])
@@ -171,14 +207,17 @@ class Offloader:
 
             # Calculate remaining time
             elapsed_time = time.time() - start_time
-            self.logger.debug("Elapsed time: %s", time.strftime('%-M min %-S sec', time.gmtime(elapsed_time)))
+            self.logger.debug("Elapsed time: %s", time.strftime(
+                '%-M min %-S sec', time.gmtime(elapsed_time)))
 
-            bytes_per_second = total_transferred_size / elapsed_time
-            self.logger.debug("Avg. transfer speed: %s/s", self.convert_size(bytes_per_second))
+            bytes_per_second = 0.00001 + total_transferred_size / elapsed_time
+            self.logger.debug("Avg. transfer speed: %s/s",
+                              self.convert_size(bytes_per_second))
 
-            size_remaining = total_file_size - total_transferred_size
-            time_remaining = size_remaining / bytes_per_second
-            self.logger.debug("Size remaining: %s", self.convert_size(size_remaining))
+            size_remaining = 0.00001 + total_file_size - total_transferred_size
+            time_remaining = 0.00001 + size_remaining / bytes_per_second
+            self.logger.debug("Size remaining: %s",
+                              self.convert_size(size_remaining))
             self.logger.debug("Approx. time remaining: %s",
                               time.strftime('%-M min %-S sec', time.gmtime(time_remaining)))
 
@@ -203,7 +242,8 @@ class Offloader:
         else:
             if incremental >= 1:
                 new_filename = "_".join([file_info['date'].strftime('%y%m%d'),
-                                         os.path.splitext(file_info['name'])[0],
+                                         os.path.splitext(
+                                             file_info['name'])[0],
                                          "{:03d}{}".format(incremental, os.path.splitext(file_info['name'])[1])])
 
             else:
@@ -211,6 +251,18 @@ class Offloader:
                                          file_info['name']])
 
         return new_filename
+
+    def compare_checksums(self, source, destination, hashtype='xxhash'):
+        source_hash = self.get_file_checksum(source)
+        dest_hash = self.get_file_checksum(destination)
+        if dest_hash == source_hash:
+            self.logger.debug(
+                'Checksums match: %s (source)| %s (destination)', source_hash, dest_hash)
+            return True
+        else:
+            self.logger.warning(
+                'Checksums mismatch: %s (source)| %s (destination)', source_hash, dest_hash)
+            return False
 
     def copy_file(self, source, destination):
         shutil.copyfile(source, destination)
@@ -241,7 +293,8 @@ class Offloader:
         for root, dirs, files in os.walk(path):
             for file in files:
                 self.logger.debug("Getting file info for %s", file)
-                file_list[file_id] = self.get_file_info(os.path.join(root, file))
+                file_list[file_id] = self.get_file_info(
+                    os.path.join(root, file))
 
                 # Append file size
                 total_file_size += file_list[file_id]['size']
@@ -250,15 +303,26 @@ class Offloader:
                 file_id += 1
 
                 self.logger.info("%s files collected", file_id - 1)
-                self.logger.debug("Total size collected: %s", self.convert_size(total_file_size))
+                self.logger.debug("Total size collected: %s",
+                                  self.convert_size(total_file_size))
         return file_list
 
-    def get_file_checksum(self, fname):
+    def get_file_checksum_md5(self, fname):
         hash_md5 = hashlib.md5()
         with open(fname, "rb") as f:
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
         return hash_md5.hexdigest()
+
+    def get_file_checksum_xxhash(self, fname):
+        hash_xxhash = xxhash.xxh64()
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_xxhash.update(chunk)
+        return hash_xxhash.hexdigest()
+
+    def get_file_checksum(self, file):
+        return self.get_file_checksum_xxhash(file)
 
     def get_file_date(self, file_path):
         return os.path.getmtime(file_path)
@@ -271,7 +335,6 @@ class Offloader:
             'path': file_path,
             'timestamp': file_timestamp,
             'date': datetime.datetime.fromtimestamp(file_timestamp),
-            'checksum': self.get_file_checksum(file_path),
             'size': os.path.getsize(file_path)
         }
         self.logger.debug("File info: %s", file_info)
@@ -281,7 +344,8 @@ class Offloader:
 def main():
     """docstring for main"""
     # Create the parser
-    cmd_parser = argparse.ArgumentParser(description='Offload files with checksum verification')
+    cmd_parser = argparse.ArgumentParser(
+        description='Offload files with checksum verification')
 
     # Add the arguments
     cmd_parser.add_argument('source',
@@ -310,7 +374,8 @@ def main():
     args = cmd_parser.parse_args()
 
     # Run offloader
-    ol = Offloader(args.source, args.destination, args.original_structure, args.move,args.dryrun)
+    ol = Offloader(args.source, args.destination,
+                   args.original_structure, args.move, args.dryrun)
     ol.offload()
 
 
--- sandbox/sync_lightroom_catalogs.py
+++ sandbox/sync_lightroom_catalogs.py
@@ -8,63 +8,89 @@ import os
 import logging
 import hashlib
 import pprint
+import json
 
 
-def get_volumes_list():
-    """returns a list of volumes"""
-    return [os.path.join('/Volumes', d) for d in os.listdir('/Volumes')]
+class LRSync:
+    def __init__(self):
+        self.catalogs = []
+        self.logger = self.setup_logger()
 
+    def setup_logger(self):
+        # Setup logger
+        logger = logging.getLogger(__name__)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)-20s %(name)-10s %(levelname)-8s %(message)s', "%Y-%m-%d %H:%M:%S")
 
-def hashfile(path, blocksize=65536):
-    file = open(path, 'rb')
-    hasher = hashlib.md5()
-    buf = file.read(blocksize)
-    while len(buf) > 0:
-        hasher.update(buf)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+    @staticmethod
+    def get_volumes_list():
+        """returns a list of volumes"""
+        return [os.path.join('/Volumes', d) for d in os.listdir('/Volumes')]
+
+    @staticmethod
+    def save_json(path, data):
+        with open(path, 'w') as outfile:
+            json.dump(data, outfile)
+
+    @staticmethod
+    def hashfile(path, blocksize=65536):
+        file = open(path, 'rb')
+        hasher = hashlib.md5()
         buf = file.read(blocksize)
-    file.close()
-    return hasher.hexdigest()
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = file.read(blocksize)
+        file.close()
+        return hasher.hexdigest()
 
+    def update_catalogs_list(self, volumes):
+        catalogs = {}
+        for vol in volumes:
+            catalogs[vol] = self.get_catalogs(vol)
 
-def get_catalogs(volume):
-    catalogs_meta = {}
-    lr_work_path = os.path.join('/Volumes', volume, 'pictures', 'lightroom', 'catalogs')
-    # Check if folder exists
-    if os.path.isdir(lr_work_path):
-        logger.debug(lr_work_path)
-        for root, dirs, files in os.walk(lr_work_path, topdown=True):
-            catalogs = [c for c in files if c.endswith('lrcat') and 'backups' not in root.lower()]
-            for cat in catalogs:
-                cat_path = os.path.join(root, cat)
-                catalogs_meta[cat] = {
-                    "path": cat_path,
-                    "rel_path": cat_path.replace(lr_work_path, ''),
-                    "last_modified": os.path.getmtime(cat_path),
-                    "md5": hashfile(cat_path)
-                }
-                logger.debug(catalogs_meta[cat])
-    logger.debug(catalogs_meta)
-    return catalogs_meta
+    def get_catalogs(self, volume):
+        catalogs_meta = {}
+        lr_work_path = os.path.join(
+            '/Volumes', volume, 'pictures', 'lightroom', 'catalogs')
+        # Check if folder exists
+        if os.path.isdir(lr_work_path):
+            self.logger.debug(lr_work_path)
+            for root, dirs, files in os.walk(lr_work_path, topdown=True):
+                catalogs = [c for c in files if c.endswith(
+                    'lrcat') and 'backups' not in root.lower()]
+                for cat in catalogs:
+                    cat_path = os.path.join(root, cat)
+                    catalogs_meta[cat] = {
+                        "path": cat_path,
+                        "rel_path": cat_path.replace(lr_work_path, ''),
+                        "last_modified": os.path.getmtime(cat_path),
+                        "md5": self.hashfile(cat_path)
+                    }
+                    self.logger.debug(catalogs_meta[cat])
+        self.logger.debug(catalogs_meta)
+        return catalogs_meta
 
 
 def main():
     """docstring for main"""
-    volumes = get_volumes_list()
-    catalogs_meta = {}
-    for vol in volumes:
-        vol = os.path.basename(vol)
-        catalogs_meta[vol] = get_catalogs(vol)
-    logger.info(catalogs_meta)
-    pprint.pprint(catalogs_meta)
-
+    # volumes = get_volumes_list()
+    # volumes = ['mcdaddy', 'mcdrive']
+    # catalogs_meta = {}
+    # for vol in volumes:
+    #     vol = os.path.basename(vol)
+    #     catalogs_meta[vol] = get_catalogs(vol)
+    #
+    # pprint.pprint(catalogs_meta)
+    lr_sync = LRSync()
+    volumes = lr_sync.get_volumes_list()
+    print(volumes)
+    catalogs = lr_sync.get_catalogs('mcdrive')
+    print(catalogs)
 
 if __name__ == '__main__':
-    # Setup logger
-    logger = logging.getLogger(__name__)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.DEBUG)
-
     main()
